a17dbd0261ebd4f1d4fa03233704f10e49ab8fe7
Flochlay <sebastien.flochlay@gmail.com>
2020-03-25 20:47:10 +0100

    chore(authentication): add apikey to firebase config file

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,15 +2,15 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "***",
+    apiKey: "AIzaSyAcBdG6zDUUDC7fbYd8t076JzzFdLk9ol4",
     authDomain: "smoothies-recipes-vuejs-demo.firebaseapp.com",
     databaseURL: "https://smoothies-recipes-vuejs-demo.firebaseio.com",
     projectId: "smoothies-recipes-vuejs-demo",
     storageBucket: "smoothies-recipes-vuejs-demo.appspot.com",
-    messagingSenderId: "***",
-    appId: "***",
-    measurementId: "***"
-};
+    messagingSenderId: "684554921411",
+    appId: "1:684554921411:web:410111c4220ba8107a8976",
+    measurementId: "G-FS30BE0QTN"
+  };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 

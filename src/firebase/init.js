4a2f2229da8d56c45a1cf14216d8b4e3704f0a7c
Flochlay <sebastien.flochlay@gmail.com>
2020-04-10 00:34:40 +0200

    chore(authentication): link this application to another firebase project

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,15 +2,15 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAcBdG6zDUUDC7fbYd8t076JzzFdLk9ol4",
-    authDomain: "smoothies-recipes-vuejs-demo.firebaseapp.com",
-    databaseURL: "https://smoothies-recipes-vuejs-demo.firebaseio.com",
-    projectId: "smoothies-recipes-vuejs-demo",
-    storageBucket: "smoothies-recipes-vuejs-demo.appspot.com",
-    messagingSenderId: "684554921411",
-    appId: "1:684554921411:web:410111c4220ba8107a8976",
-    measurementId: "G-FS30BE0QTN"
-}
+    apiKey: "AIzaSyDAy4xSFuAO9LOqW5BXMSmwScDcw8emVcE",
+    authDomain: "vue-workshop-applications.firebaseapp.com",
+    databaseURL: "https://vue-workshop-applications.firebaseio.com",
+    projectId: "vue-workshop-applications",
+    storageBucket: "vue-workshop-applications.appspot.com",
+    messagingSenderId: "1042184740644",
+    appId: "1:1042184740644:web:4918d23c56883fc7461db1",
+    measurementId: "G-MVBBWMRSTY"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
